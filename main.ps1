3db1d0f3e62310dc5353d5169992cf3e707e6b60
Young-Gi Park <ghostyak@gmail.com>
2025-04-27 21:49:53 +0900

    fix: main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,5 @@
-& ".\01-extract-TerminalServices-LocalSessionManager-Operational.ps1 -i Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx -o Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.csv"
-& ".\02-extract-TerminalServices-RDPClient-Operational.ps1 -i Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx -o Microsoft-Windows-TerminalServices-RDPClient%4Operational.csv"
-& ".\03-extract-RemoteDesktopServices-RdpCoreTS-Operational.ps1 -i Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx -o Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.csv"
-& ".\04-extract-TerminalServices-RemoteConnectionManager-Operational.ps1 -i Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx -o Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.csv"
-& ".\05-extract-Security.ps1 -i Security.evtx -o Security.csv"
+& .\01-extract-TerminalServices-LocalSessionManager-Operational.ps1 -i Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx -o Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.csv
+& .\02-extract-TerminalServices-RDPClient-Operational.ps1 -i Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx -o Microsoft-Windows-TerminalServices-RDPClient%4Operational.csv
+& .\03-extract-RemoteDesktopServices-RdpCoreTS-Operational.ps1 -i Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx -o Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.csv
+& .\04-extract-TerminalServices-RemoteConnectionManager-Operational.ps1 -i Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx -o Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.csv
+& .\05-extract-Security.ps1 -i Security.evtx -o Security.csv
